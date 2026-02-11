5a61dda1c0be35544c85603aece1203221e20eb2
John Leon <jleon253@gmail.com>
2026-02-11 11:40:31 -0500

    Added main app files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,37 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
+
+const toastrConfig = {
+  timeOut: 3000,
+  positionClass: 'toast-top-right',
+  preventDuplicates: true,
+  closeButton: true,
+  progressBar: true,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideToastr(toastrConfig),
+    provideNoopAnimations(),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({ prefix: 'i18n/', suffix: '.json' }),
+      fallbackLang: 'es',
+    }),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Layout } from "./shared/components/layout/layout";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', redirectTo: 'customers', pathMatch: 'full' },
+      {
+        path: 'customers',
+        loadComponent: () => import('./features/customers/customer-management/customer-management').then(m => m.CustomerManagement)
+      },
+      {
+        path: 'accounts',
+        loadComponent: () => import('./features/accounts/account-management/account-management').then(m => m.AccountManagement)
+      }
+    ]
+  },
+  { path: '**', redirectTo: 'customers' }
+];
